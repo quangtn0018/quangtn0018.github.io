93ca62fdbe80c9f8fc9705206b08170197d38c1b
Quang Nguyen <quangtn0018@gmail.com>
2018-03-26 12:43:49 -0700

    Updates

--- src/components/container.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import containerStyles from "./containerStyles.module.scss";
-
-export default ({ children }) => (
-  <div className={containerStyles.container}>{children}</div>
-);
